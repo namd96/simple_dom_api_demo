b02b9a71acf00229da848966c3cad32689905ee6
namrata <dnamrata0805@gmail.com>
2018-11-18 16:58:09 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var a = 80;
+console.log(a);
+
+var rightDiv = document.getElementsByClassName("rightClass")[0];
+
+var showBtn = document.getElementById("showBtn");
+showBtn.addEventListener("click", () => {
+    var bazinga = document.createElement('span');
+    rightDiv.style.visibility = "visible";
+    // rightDiv.innerHTML = bazinga;
+    rightDiv.innerHTML = "";
+    rightDiv.appendChild(bazinga);
+    bazinga.innerHTML = "Bazingaa";
+    bazinga.addEventListener("mouseenter" , ()=> {
+        bazinga.innerHTML = "I am sheldon";
+        bazinga.style.fontSize = "20px";
+    })
+    bazinga.addEventListener("mouseleave" , ()=> {
+        bazinga.style.fontSize = "unset";
+        bazinga.innerHTML = "Bazingaa";
+    })
+})
+
+function hint() {
+    alert("mouse over bazinga");
+}
+
+
+function hide (){
+    rightDiv.style.visibility = "hidden";
+}
